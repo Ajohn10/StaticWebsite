8c923355290049bceb61014a81368c7d562d06bc
Adam Johnson <87151710+Ajohn10@users.noreply.github.com>
2025-03-12 22:10:24 -0400

    made skill icons more aesthetically pleasing

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,7 +16,7 @@ function Home() {
                         <div className="w-full justify-start items-center gap-8 grid lg:grid-cols-3 grid-cols-2">
                             {skills.map((skill, index) =>
                                 <div key={index}
-                                    className="w-full h-full p-3.5 rounded-xl bg-opacity-50 bg-gray-800 hover:bg-opacity-100 text-gray-900 hover:text-slate-300 transition-all duration-700 ease-in-out flex-col justify-start items-start gap-2.5 inline-flex">
+                                    className="w-full h-full p-3.5 rounded-xl hover:rounded-2xl bg-opacity-70 bg-gray-900 hover:bg-opacity-100 text-slate-200 transition-all duration-700 ease-in-out flex-col justify-start items-start gap-2.5 inline-flex hover:pl-5">
                                     <h4 className="text-md md:text-2xl font-bold font-manrope leading-9">{skill}</h4>
                                 </div>
                             )}
